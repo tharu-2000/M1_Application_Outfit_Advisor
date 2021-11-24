46d7333447a51da6b3bc13c631ba4102fa62fec5
tharu-2000 <55780247+tharu-2000@users.noreply.github.com>
2021-11-24 20:44:07 +0530

    added header

---
+++ 3_Implementation/inc/outfit.h
@@ -0,0 +1,27 @@
+/**
+ * @file Outfit_Advisor.h
+ * @author Tharageshwari (thara@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/*
+define macro OUTFIT_ADVISOR, this will reduce multiple calling
+of the header file
+*/
+
+#ifndef OUTFIT_ADVISOR
+#define OUTFIT_ADVISOR
+
+/*function declarations*/
+
+float subract(int x, int y)
+{
+    return(x - y);
+};
+
+#endif 
